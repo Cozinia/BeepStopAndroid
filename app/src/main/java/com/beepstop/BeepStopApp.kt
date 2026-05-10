f7c3bfb453cb18f8c46550f0983bfc6dcfcb3598
Cozinia Motronu <hdwallpaper04@gmail.com>
2026-05-10 20:03:00 +0300

    add workmanager race calendar worker

--- app/src/main/java/com/beepstop/BeepStopApp.kt
+++ app/src/main/java/com/beepstop/BeepStopApp.kt
@@ -6,10 +6,18 @@ import com.beepstop.data.local.PreferencesManager
 import com.beepstop.data.remote.RetrofitClient
 import com.beepstop.data.repository.RaceRepository
 import com.beepstop.data.repository.StandingsRepository
+import com.beepstop.worker.RaceCalendarWorker
+import com.beepstop.worker.StandingsWorker
 
 class BeepStopApp : Application() {
     val database by lazy { AppDatabase.getInstance(this) }
     val raceRepository by lazy { RaceRepository(RetrofitClient.api, database.raceDao()) }
     val standingsRepository by lazy { StandingsRepository(RetrofitClient.api, database.constructorDao(), database.driverDao()) }
     val preferencesManager by lazy { PreferencesManager(this) }
+
+    override fun onCreate() {
+        super.onCreate()
+        RaceCalendarWorker.schedule(this)
+        StandingsWorker.schedule(this)
+    }
 }
